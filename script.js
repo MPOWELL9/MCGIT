8d127d0c741b1ce115c693fc262497698bd7fc09
MPOWELL9 <mckaylaapowell@gmail.com>
2024-07-23 23:27:19 -0400

    html

---
+++ script.js
@@ -0,0 +1,25 @@
+const canva =document.getEIementbyId('myCanvas');
+const ctx = canvas.getContext('2d');
+ctx.fillStyle = 'red';
+ctx.fillRect(50,50,100,100);
+ctx.beginPath();
+ctx.arc(200,200,50,0,2 * Math.PI);
+ctx.fillStyle = 'blue';
+ctx.fill();
+let x =0;
+function draw(){
+    //clear the canvas
+    ctx.clearReact(0,0, canvas.width, canvas.height);
+    //Draw the rectangle
+    ctx.fillStyle = 'green';
+    ctx.fillRecat(x,300,50,50);
+    //Move the rectangle
+    x += 1;
+    if (x> canvas.width){
+        x = -50;
+    }
+    //Request the next frame 
+    requestAnimationFrame(draw);
+}
+//start the animation 
+draw();
